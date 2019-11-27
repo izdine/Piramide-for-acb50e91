a41bc32b204c8168778ccd251294c9999acd624f
Iz-Dine Amadou <2088346@talnet.nl>
2019-11-27 13:51:06 +0100

    Uitwerking van de exercise

--- piramide.php
+++ piramide.php
@@ -1,10 +1,10 @@
 <?php
-echo "Hoeveel stapels wil je hebben?:\n". PHP_EOL;
+echo "Hoeveel stapels wil je hebben?:\n";
 $e = readline ();
 for ($i=1; $i < $e; $i++) {
   for ($j= 1; $j <= $i; $j++) {
 
-    echo "=";
+    echo "*";
   }
 echo ''. PHP_EOL;
 
